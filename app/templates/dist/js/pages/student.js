1bd54fbc6d7b916fd57a18b4312b8c1d06b2f7bc
giangcse <giangphan028@gmail.com>
2024-01-25 19:33:11 +0700

    fixxxxx

--- app/templates/dist/js/pages/student.js
+++ app/templates/dist/js/pages/student.js
@@ -85,22 +85,24 @@ if (document.cookie.indexOf('studentid') == -1){
                 "Content-Type": "application/json"
                 },
             success: function(res){
-                clear_modal();
-                $("#modal_title").text('Xác thực OTP');
-                html = '<div class="form-group"> <label>OTP:</label><input type="number" id="otp"/></div>'
-                $("#modal_body").append(html);
-                $("#modal_footer").append(
-                    '<button type="button" class="btn btn-primary" id="modal_submit_btn"><i class="fa-solid fa-floppy-disk"></i> Xác thực</button>'
-                );
-                $("#modal_id").modal('show');
-
-                $("#modal_submit_btn").on('click', function(){
-                    let email = $('#sinhvien_email').val();
-                    let otp = $('#otp').val();
-
-                    xac_thuc_otp(email, otp);
-                    $('#modal_id').modal('hide');
-                });
+                if(res.status=='OK'){
+                    clear_modal();
+                    $("#modal_title").text('Xác thực OTP');
+                    html = '<div class="form-group"> <label>OTP:</label><input type="number" id="otp"/></div>'
+                    $("#modal_body").append(html);
+                    $("#modal_footer").append(
+                        '<button type="button" class="btn btn-primary" id="modal_submit_btn"><i class="fa-solid fa-floppy-disk"></i> Xác thực</button>'
+                    );
+                    $("#modal_id").modal('show');
+    
+                    $("#modal_submit_btn").on('click', function(){
+                        let email = $('#sinhvien_email').val();
+                        let otp = $('#otp').val();
+    
+                        xac_thuc_otp(email, otp);
+                        $('#modal_id').modal('hide');
+                    });
+                }
 
                 disable_input();
             },
