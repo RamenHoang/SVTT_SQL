86870b4e47fc5be3b56fe703c2f4134e4534496e
giangcse <giangphan028@gmail.com>
2024-01-26 15:25:13 +0700

    Fix bugs

--- app/templates/dist/js/pages/sv_index.js
+++ app/templates/dist/js/pages/sv_index.js
@@ -6,57 +6,64 @@ var Toast = Swal.mixin({
 });
 
 $(document).ready(function() {
-    let cookie = document.cookie;
-    let email = cookie.split('username=')[1].replaceAll('"', '');
+    let cookie = document.cookie.split(';');
     let bangthongtin = $('#bang_thongtinsinhvien tbody');
-    $.ajax({
-        type: 'GET',
-        url: '/xem_thong_tin_sv?email='+email,
-        success: function(res){
-            html = `
-                <tr>
-                    <td>MSSV:</td>
-                    <td>${res.mssv}</td>
-                </tr>
-                <tr>
-                    <td>Họ tên:</td>
-                    <td>${res.hoten}</td>
-                </tr>
-                <tr>
-                    <td>Giói tính:</td>
-                    <td>${res.gioitinh == 1 ? 'Nam' : 'Nữ'}</td>
-                </tr>
-                <tr>
-                    <td>Số điện thoại:</td>
-                    <td>${res.sdt}</td>
-                </tr>
-                <tr>
-                    <td>Email:</td>
-                    <td>${res.email}</td>
-                </tr>
-                <tr>
-                    <td>Địa chỉ:</td>
-                    <td>${res.diachi}</td>
-                </tr>
-                <tr>
-                    <td>Mã lớp:</td>
-                    <td>${res.malop}</td>
-                </tr>
-                <tr>
-                    <td>Ngành:</td>
-                    <td>${res.nganh}</td>
-                </tr>
-                <tr>
-                    <td>Trường:</td>
-                    <td>${res.truong}</td>
-                </tr>
-                <tr>
-                    <td>Nhóm:</td>
-                    <td>${res.nhomhuongdan===null ? '' : res.nhomhuongdan}</td>
-                </tr>
-            `;
 
-            bangthongtin.append(html);
+    cookie.forEach(function(val){
+        if(val.includes(" username=")){
+            let email = val.split('username=')[1].replaceAll('"','');
+            
+            $.ajax({
+                type: 'GET',
+                url: '/xem_thong_tin_sv?email='+email,
+                success: function(res){
+                    html = `
+                        <tr>
+                            <td>MSSV:</td>
+                            <td>${res.mssv}</td>
+                        </tr>
+                        <tr>
+                            <td>Họ tên:</td>
+                            <td>${res.hoten}</td>
+                        </tr>
+                        <tr>
+                            <td>Giói tính:</td>
+                            <td>${res.gioitinh == 1 ? 'Nam' : 'Nữ'}</td>
+                        </tr>
+                        <tr>
+                            <td>Số điện thoại:</td>
+                            <td>${res.sdt}</td>
+                        </tr>
+                        <tr>
+                            <td>Email:</td>
+                            <td>${res.email}</td>
+                        </tr>
+                        <tr>
+                            <td>Địa chỉ:</td>
+                            <td>${res.diachi}</td>
+                        </tr>
+                        <tr>
+                            <td>Mã lớp:</td>
+                            <td>${res.malop}</td>
+                        </tr>
+                        <tr>
+                            <td>Ngành:</td>
+                            <td>${res.nganh}</td>
+                        </tr>
+                        <tr>
+                            <td>Trường:</td>
+                            <td>${res.truong}</td>
+                        </tr>
+                        <tr>
+                            <td>Nhóm:</td>
+                            <td>${res.nhomhuongdan===null ? '' : res.nhomhuongdan}</td>
+                        </tr>
+                    `;
+        
+                    bangthongtin.append(html);
+                }
+            });
         }
-    });
+    })
+
 });
