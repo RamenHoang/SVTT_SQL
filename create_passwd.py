8d4ca87fa4d999ab24e331d354e20403e99a1c9b
giangcse <giangphan028@gmail.com>
2024-04-10 17:03:07 +0700

    Chuyển secret_key và algorithm vào .env

--- create_passwd.py
+++ create_passwd.py
@@ -1,7 +1,8 @@
 from hashlib import sha3_256
+from .app.config import secret_key, algorithm
 
-SECRET_KEY = "BN3298"
-ALGORITHM = "HS256"
+SECRET_KEY = secret_key
+ALGORITHM = algorithm
 
 passwd = input("Type password: ")
 print(sha3_256(bytes(passwd, 'utf-8')).hexdigest())
